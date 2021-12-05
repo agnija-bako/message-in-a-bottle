30ad1ab6373ea1bc532a7ccd79f4ad078870a916
Agnija <agnijabako@yahoo.com>
2021-12-05 17:24:53 +0100

    cleanup unused components

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,15 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 
 const rootElement = document.getElementById('root');
 
-ReactDOM.render(
-    <App />,rootElement);
+ReactDOM.render(<App />,rootElement);
 
-registerServiceWorker();
 
